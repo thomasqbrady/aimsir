20fe31f5d8f8b4da61575bd2e539de96d10d2626
Thomas Q Brady <tqb@thomasqbrady.com>
2015-01-02 21:56:01 -0600

    Really fixed timezone?

--- routes/commands.js
+++ routes/commands.js
@@ -1,23 +1,21 @@
 var express = require('express');
 var router = express.Router();
-var time = require('time');
 
 var commands = [];
 
 function getTime() {
-    var now = new time.Date();
-    now.setTimezone('America/Chicago');
-    var h = now.getHours();
+    var date = new Date();
+    var h = date.getHours();
     console.log("initial h:"+h);
     h += 6;
     console.log("adjusted h:"+h);
     if (h > 12) { h -= 12 }
     console.log("non-military h:"+h);
-    var m = now.getMinutes();
+    var m = date.getMinutes();
     if (m < 10) {
         m = "0" + m;
     }
-    var s = now.getSeconds();
+    var s = date.getSeconds();
     if (s % 2 == 0) {
         return "t" + h + ":" + m
     } else {
