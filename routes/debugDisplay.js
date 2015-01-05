7b336b6f1917d2eb33d83f5a69f6aaea714eaffe
Thomas Q Brady <tqb@thomasqbrady.com>
2015-01-05 13:17:54 -0600

    Remove degrees?

---
+++ routes/debugDisplay.js
@@ -0,0 +1,24 @@
+var SerialPort = require("serialport").SerialPort
+var serialPort = new SerialPort("/dev/tty.usbserial-A603Q3QZ", {
+  baudrate: 115200
+}, false); // this is the openImmediately flag [default is true]
+
+serialPort.open(function (error) {
+  if ( error ) {
+    console.log('failed to open: '+error);
+  } else {
+    console.log('open');
+    serialPort.on('data', function(data) {
+      console.log('data received: ' + data);
+    });
+    setInterval(function(){
+      var d = new Date();
+      var separator = (d.getSeconds()%2 == 0) ? ":" : " ";
+      var timestamp = "t"+d.getHours()+separator+d.getMinutes+"w90"+String.fromCharCode(176)+"F";
+      serialPort.write(timestamp+"\n", function(err, results) {
+        console.log('err ' + err);
+        console.log('results ' + results);
+      });
+    },1000);
+  }
+});
